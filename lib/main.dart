ee9e5c8107cd9eec310999f58cff406fd6f2ade9
samisnotinsane <sameen11@hotmail.com>
2020-06-24 16:51:58 +0100

    Adding play/stop to hardcoded stream.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:flutter_sound_lite/flutter_sound_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,18 +20,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BoringPage extends StatefulWidget {
-  @override
-  _BoringPageState createState() => _BoringPageState();
-}
-
-class _BoringPageState extends State<BoringPage> {
+class BoringPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('This is sooo boring...'),
+        child: PlaybackButton(),
       ),
+    );
+  }
+}
+
+class PlaybackButton extends StatefulWidget {
+  @override
+  _PlaybackButtonState createState() => _PlaybackButtonState();
+}
+
+class _PlaybackButtonState extends State<PlaybackButton> {
+  bool _isPlaying = false;
+  FlutterSoundPlayer _sound;
+
+  void _stop() async {
+    await _sound.stopPlayer();
+  }
+
+  void _play() async {
+    final url = 'https://s3-us-west-2.amazonaws.com/anchor-audio-bank/production/2020-2-8/55332291-44100-2-920107b8952c8.mp3';
+    await _sound.startPlayer(url);
+  }
+
+
+  @override
+  void initState() { 
+    super.initState();
+    _sound = new FlutterSoundPlayer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
+      onPressed: () {
+        if (_isPlaying) {
+          _stop();
+        } else {
+          _play();
+        }
+        setState(() => _isPlaying = !_isPlaying);
+      },
     );
   }
 }
