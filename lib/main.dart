374b490bf3c1d1fe80b17a067c1c648b6f773099
samisnotinsane <sameen11@hotmail.com>
2020-06-24 19:00:33 +0100

    End of episode 19.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_sound_lite/flutter_sound.dart';
 import 'package:flutter_sound_lite/flutter_sound_player.dart';
 
 void main() {
@@ -24,9 +25,50 @@ class BoringPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: PlaybackButton(),
+      body: SafeArea(
+        child: DashCastApp(),
       ),
+    );
+  }
+}
+
+class DashCastApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          flex: 9,
+          child: Placeholder(),
+        ),
+        Flexible(
+          flex: 2,
+          child: AudioControls(),
+        ),
+      ],
+    );
+  }
+}
+
+class AudioControls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        PlaybackButtons(),
+      ],
+    );
+  }
+}
+
+class PlaybackButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        PlaybackButton(),
+      ],
     );
   }
 }
@@ -39,35 +81,73 @@ class PlaybackButton extends StatefulWidget {
 class _PlaybackButtonState extends State<PlaybackButton> {
   bool _isPlaying = false;
   FlutterSoundPlayer _sound;
+  double _playPosition;
+  Stream<PlayStatus> _playerSubscription;
 
   void _stop() async {
+    print('Stopping');
     await _sound.stopPlayer();
+    setState(() => _isPlaying = false);
   }
 
   void _play() async {
-    final url = 'https://s3-us-west-2.amazonaws.com/anchor-audio-bank/production/2020-2-8/55332291-44100-2-920107b8952c8.mp3';
+    final url =
+        'https://s3-us-west-2.amazonaws.com/anchor-audio-bank/production/2020-2-8/55332291-44100-2-920107b8952c8.mp3';
     await _sound.startPlayer(url);
+
+    _playerSubscription = _sound.onPlayerStateChanged
+      ..listen((e) {
+        if (e != null) {
+          _playPosition = e.currentPosition;
+          if (_playPosition != 0) {
+            print(e.currentPosition);
+          }
+          setState(() => _playPosition = e.currentPosition / e.duration);
+        }
+      });
+    setState(() => _isPlaying = true);
   }
 
+  void _fastForward() {}
+
+  void _rewind() {}
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     _sound = new FlutterSoundPlayer();
+    _playPosition = 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
-      onPressed: () {
-        if (_isPlaying) {
-          _stop();
-        } else {
-          _play();
-        }
-        setState(() => _isPlaying = !_isPlaying);
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Slider(
+          value: _playPosition,
+          onChanged: (newValue) {
+            _playPosition = newValue;
+          },
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(icon: Icon(Icons.fast_rewind), onPressed: null),
+            IconButton(
+              icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
+              onPressed: () {
+                if (_isPlaying) {
+                  _stop();
+                } else {
+                  _play();
+                }
+              },
+            ),
+            IconButton(icon: Icon(Icons.fast_forward), onPressed: null),
+          ],
+        ),
+      ],
     );
   }
 }
